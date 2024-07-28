f482fbdac654cb82aea4c8bb4e273565d2d64468
Sergei <106452081+Fidyi@users.noreply.github.com>
2024-07-28 14:37:46 +0300

    feat: setup tests

---
+++ jest.config.ts
@@ -0,0 +1,20 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.app.json',
+    },
+  },
+};
---
+++ src/__tests__/App.spec.tsx
@@ -0,0 +1,17 @@
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import App from '../App';
+
+test('demo', () => {
+  expect(true).toBe(true);
+});
+
+test('Renders the main page', () => {
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
+  expect(true).toBeTruthy();
+});
